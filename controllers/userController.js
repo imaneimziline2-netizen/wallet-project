d6d3497505ec39efbdd177b0e0e6fd3c781f71a6
imane imziline <imaneimzline2@gmail.com>
2026-02-19 12:41:12 +0000

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,38 @@ exports.createUser =(req,res,body) => {
     const newUser = {
         id: users.length +1,
         name: body.name
-    }
+    };
+    // tableau fih  uesers kanzido fih newUser
+    users.push(newUser);
+    // writeHead katsayb lina harder li fih status code o content typ 
+res.writeHead(201,{'content-type':'application/json'});
+// katift lina nresprnce
+res.end(JSON.stringify(newUser));
 };
-users.push(newUser);
-res.writeHead()
+
+// READ
+exports.getUser=(req,res)=>{
+    res.writeHead(200,{'content-type':'application/json'});
+res.end(JSON.stringify(users));
+};
+
+// Update
+exports.updateUser=(req,res,id,body)=>{
+   const user = users.find(u => u.id == id);
+    if(!user){
+        res.writeHead(404);
+        return res.end('user not found')
+    }
+    user.name = body.name;
+    res.writeHead(200,{
+        'content-typ':'application/json'
+    });
+    res.end(JSON.stringify(user))
+};
+
+// delet
+exports.deletUser =(req,res,id)=>{
+    users = users.filter(u=>u.id != id);
+    res.writeHead(200);
+    res.end("User deleted")
+}
